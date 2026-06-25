44fb603afecc95086f9bd30ad4c0c6f9c0dab989
MiguelMussalam <miguelmussalam@gmail.com>
2026-06-24 22:18:47 -0300

    verificacao de compatibilidade das fontes utilizadas

--- src/helper/fonts.py
+++ src/helper/fonts.py
@@ -1,21 +1,24 @@
 import os
 import requests
+from fontTools.ttLib import TTFont
+from kanjis import get_kanjis
 
 FONTES = {
-    "BIZ-UDPGothic-Regular":  "https://raw.githubusercontent.com/google/fonts/main/ofl/bizudpgothic/BIZUDPGothic-Regular.ttf",
-    "BIZ-UDPGothic-Bold":     "https://raw.githubusercontent.com/google/fonts/main/ofl/bizudpgothic/BIZUDPGothic-Bold.ttf",
-    "BIZ-UDPMincho-Regular":  "https://raw.githubusercontent.com/google/fonts/main/ofl/bizudpmincho/BIZUDPMincho-Regular.ttf",
-    "Klee-One-Regular":       "https://raw.githubusercontent.com/google/fonts/main/ofl/kleeone/KleeOne-Regular.ttf",
-    "Klee-One-SemiBold":      "https://raw.githubusercontent.com/google/fonts/main/ofl/kleeone/KleeOne-SemiBold.ttf",
-    "Hina-Mincho-Regular":    "https://raw.githubusercontent.com/google/fonts/main/ofl/hinamincho/HinaMincho-Regular.ttf",
-    "Yusei-Magic-Regular":    "https://raw.githubusercontent.com/google/fonts/main/ofl/yuseimagic/YuseiMagic-Regular.ttf",
-    "Dela-Gothic-One":        "https://raw.githubusercontent.com/google/fonts/main/ofl/delagothicone/DelaGothicOne-Regular.ttf",
-    "Reggae-One":             "https://raw.githubusercontent.com/google/fonts/main/ofl/reggaeone/ReggaeOne-Regular.ttf",
+    "Shippori Antique":         "https://raw.githubusercontent.com/fontdasu/ShipporiAntique/master/fonts/ttf/ShipporiAntique-Regular.ttf",
+    "BIZ-UDPGothic-Regular":    "https://raw.githubusercontent.com/google/fonts/main/ofl/bizudpgothic/BIZUDPGothic-Regular.ttf",
+    "BIZ-UDPGothic-Bold":       "https://raw.githubusercontent.com/google/fonts/main/ofl/bizudpgothic/BIZUDPGothic-Bold.ttf",
+    "BIZ-UDPMincho-Regular":    "https://raw.githubusercontent.com/google/fonts/main/ofl/bizudpmincho/BIZUDPMincho-Regular.ttf",
+    "Klee-One-Regular":         "https://raw.githubusercontent.com/google/fonts/main/ofl/kleeone/KleeOne-Regular.ttf",
+    "Klee-One-SemiBold":        "https://raw.githubusercontent.com/google/fonts/main/ofl/kleeone/KleeOne-SemiBold.ttf",
+    "Hina-Mincho-Regular":      "https://raw.githubusercontent.com/google/fonts/main/ofl/hinamincho/HinaMincho-Regular.ttf",
+    "Yusei-Magic-Regular":      "https://raw.githubusercontent.com/google/fonts/main/ofl/yuseimagic/YuseiMagic-Regular.ttf",
+    "Dela-Gothic-One":          "https://raw.githubusercontent.com/google/fonts/main/ofl/delagothicone/DelaGothicOne-Regular.ttf",
+    "Reggae-One":               "https://raw.githubusercontent.com/google/fonts/main/ofl/reggaeone/ReggaeOne-Regular.ttf",
 }
 
 DIRETORIO_SAIDA = os.path.join("assets", "fonts")
 
-def baixar_fontes(fontes, diretorio_destino):
+def download_fonts(fontes, diretorio_destino):
     os.makedirs(diretorio_destino, exist_ok=True)
 
     for nome, url in fontes.items():
@@ -36,5 +39,22 @@ def baixar_fontes(fontes, diretorio_destino):
         except requests.exceptions.RequestException as e:
             print(f"erro: {nome}: {e}")
 
+def verify_fonts_compatibility():
+    kanjis_n1 = get_kanjis("n1")
+    for fonts in os.listdir(DIRETORIO_SAIDA):
+        kanjis_not_supported = []
+        font = TTFont(os.path.join(DIRETORIO_SAIDA, fonts))
+        font_cmap = font.getBestCmap()
+        for kanji in kanjis_n1:
+            if ord(kanji) not in font_cmap:
+                kanjis_not_supported.append(kanji)
+        if kanjis_not_supported:
+            print(f"Fonte {fonts} não suporta os seguintes kanjis: {', '.join(kanjis_not_supported)}")
+        else:
+            print(f"Fonte {fonts} suporta todos os kanjis N1")
+
+
+
 if __name__ == "__main__":
-    baixar_fontes(FONTES, DIRETORIO_SAIDA)
+    download_fonts(FONTES, DIRETORIO_SAIDA)
+    verify_fonts_compatibility()
